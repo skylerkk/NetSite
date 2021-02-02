71c7838a25d2c30ba234a9f057f0a97ed37dd3aa
skylerkk <kuretza.skyler@gmail.com>
2021-02-02 12:42:23 -0500

    added styling and routing

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { AccountService } from './../_services/account.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map(user => {
+        if (user) return true;
+        this.toastr.error('What are you doing here? You arent supposed to be here. Go away!')
+      })
+    );
+  }
+  
+}
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from './../_services/account.service';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelregister = new EventEmitter();
   model:any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +22,7 @@ export class RegisterComponent implements OnInit {
       this.cancel();
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
     
   }
