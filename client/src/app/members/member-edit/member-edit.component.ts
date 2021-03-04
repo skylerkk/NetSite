6884c91d316d53217a535d1189e7777e8cb33b8b
skylerkk <kuretza.skyler@gmail.com>
2021-03-04 11:09:41 -0500

    Adding Profile Editing

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,10 +1,10 @@
-import { ToastrService } from 'ngx-toastr';
-import { MembersService } from './../../_services/members.service';
-import { AccountService } from './../../_services/account.service';
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
 import { take } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -16,14 +16,15 @@ export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
-  @HostListener('window:beforeunload', ['$event']) unloadNotification($event:any){
-    if (this.editForm.dirty){
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm.dirty) {
       $event.returnValue = true;
     }
   }
 
-  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  constructor(private accountService: AccountService, private memberService: MembersService, 
+    private toastr: ToastrService) { 
+      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
   }
 
   ngOnInit(): void {
@@ -38,9 +39,8 @@ export class MemberEditComponent implements OnInit {
 
   updateMember() {
     this.memberService.updateMember(this.member).subscribe(() => {
-      this.toastr.success("Profile Updated Succesfully");
+      this.toastr.success('Profile updated successfully');
       this.editForm.reset(this.member);
     })
   }
-
 }
